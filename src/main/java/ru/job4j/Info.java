49a004cf86fe3a88072997715c2884900f917a2a
macbook <79824177721@yandex.ru>
2023-01-23 20:04:31 +0500

    4.1. Аргументы [#295521]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -3,5 +3,8 @@ package ru.job4j;
 public class Info {
     public static void main(String[] args) {
         System.out.println("23.01.2022");
+        int size = 10;
+        size = 100;
+        size = size - 5;
     }
 }
