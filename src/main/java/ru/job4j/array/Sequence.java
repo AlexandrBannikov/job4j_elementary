3b70fbdd91cb7a3306ff02eac165e2e616689243
macbook <79824177721@yandex.ru>
2023-02-01 19:54:58 +0500

    6.6.0. Двумерный массив [#226796]

--- src/main/java/ru/job4j/array/Sequence.java
+++ src/main/java/ru/job4j/array/Sequence.java
@@ -9,8 +9,8 @@ public class Sequence {
         array[1][0] = 4;
         array[1][1] = 5;
         array[1][2] = 6;
-        array[2][0] = 1;
-        array[2][1] = 1;
-        array[2][2] = 1;
+        array[2][0] = 7;
+        array[2][1] = 8;
+        array[2][2] = 9;
     }
 }
