c7611ccc3833bfa899fb0b1f412f0ad8ea23cc82
Alexandr Bannikov <7932417721@mail.ru>
2024-12-30 10:34:25 +0500

    3. Тестовое задание. Блок "Базовый синтаксис" [#505049]

---
+++ src/main/java/ru/job4j/condition/SimpleStringEncoder.java
@@ -0,0 +1,20 @@
+package ru.job4j.condition;
+
+public class SimpleStringEncoder {
+    public static String encode(String input) {
+        String result = "";
+        char symbol = input.charAt(0);
+        int counter = 1;
+        for (int i = 1; i < input.length(); i++) {
+            if (input.charAt(i) == symbol) {
+                counter++;
+            } else {
+                result += symbol + Integer.toString(counter);
+                symbol = input.charAt(i);
+                counter = 1;
+            }
+        }
+        result += symbol + Integer.toString(counter);
+        return result;
+    }
+}
